fda7d60398cef83007a5ff52e8f34d7f177ab4cc
Christian CHukwu <christianchukwu@MacBook-Pro.local>
2022-10-29 10:04:51 +0100

    link list completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch
-} from 'react-router-dom'
+import React, {Component} from 'react';
 import LinkList from './components/LinkList';
+import './App.css'
+import './components/LinkList.css'
 
-const App = () => {
-  return <>
-    <main>
-      Hello this is a test text!
-      <LinkList/>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        {
+        link: 'https://twitter.com/chriscrea8',
+        item: 'Twitter Link',
+        id: 1
+      },
+      {
+        link: 'https://training.zuri.team/',
+        item: 'Zuri Team',
+        id: 2
+      },
+      {
+        link: 'http://books.zuri.team ',
+        item: 'Zuri Books',
+        id: 3
+      },
+      {
+        link: 'https://books.zuri.team/python-for-beginners?ref_id=chriscrea8',
+        item: 'Python Books',
+        id: 4
+      },
+      {
+        link: 'https://background.zuri.team',
+        item: 'Background Check for Coders',
+        id: 5
+      },
+      {
+        link: 'https://books.zuri.team/design-rules',
+        item: 'Design Books',
+        id: 6
+      }
+      ]
+    }
+  }
 
-    </main>
-  </>
+  render() {
+  return (
+    <div className='App'>
+      <LinkList> 
+        {this.state.items.map(item => (
+          <a href={item.link} key={item.id} className="link">{item.item}</a>
+        ))}
+      </LinkList>
+      
+    </div>
+  )
+  }
 }
 
 export default App;
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,27 +1,12 @@
-import React, {useState, useEffect} from 'react'
-import {ListGroup, Image} from 'react-bootstrap';
-import LinkButton from './LinkButton';
+import React from 'react'
 import './LinkList.css'
 
-const LinkList = () => {
-    const links = [
-        'https://twitter.com/chriscrea8',
-        'https://training.zuri.team/',
-        'http://books.zuri.team ',
-        'https://books.zuri.team/python-for-beginners?ref_id=chriscrea8',
-        'https://background.zuri.team',
-        'https://books.zuri.team/design-rules',
-    ]
-  return <ListGroup>
-    <div>
-        <Image id='profile__img' alt= "profile-image" width="120" height="120" src={`${process.env.PUBLIC_URL}/profile.jpg`} />
-
-        <h5 className='profile-name'>Christian Chukwu</h5>
-        {links.map((link) => (
-            <LinkButton key={link} link={link}/>
-        ))}
+function LinkList(props) {
+  return (
+    <div className='link-list'>
+      {props.children}
     </div>
-  </ListGroup>
+  )
 }
 
 export default LinkList
